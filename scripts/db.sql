bc117d5aa81c6597418124dcfd614eb250ddba95
Bilal Bentoumi <bilalbentoumi@gmail.com>
2023-09-11 17:40:32 +0100

    Update database script

--- scripts/db.sql
+++ scripts/db.sql
@@ -8,3 +8,34 @@ create table user
     user_create_date VARCHAR(255) null,
     user_update_date VARCHAR(255) null
 );
+
+create table category
+(
+    category_id int auto_increment primary key,
+    category_name VARCHAR(255) null,
+    category_link VARCHAR(255) null,
+    category_description VARCHAR(255) null,
+    category_status int null,
+    category_create_date VARCHAR(255) null,
+    category_update_date VARCHAR(255) null
+);
+
+create table category_field
+(
+    field_id int auto_increment primary key,
+    category_id int,
+    field_type int,
+    field_name VARCHAR(255) null,
+    field_label VARCHAR(255) null,
+    field_required int null,
+    field_create_date VARCHAR(255) null
+);
+
+create table category_field_option
+(
+    option_id int auto_increment primary key,
+    field_id int,
+    option_name VARCHAR(255) null,
+    option_value VARCHAR(255) null,
+    option_create_date VARCHAR(255) null
+);
